2cd4b131447e71720d07bc6adec0c0b666b2d234
Evgeny Kabardinsky <ekabardinsky@gmail.com>
2019-08-09 23:52:02 +0500

    Sync sent messages

--- index.js
+++ index.js
@@ -14,19 +14,52 @@ const password = process.env.SKYPE_PASSWORD;
         }
     });
 
+    const me = api.context.username;
+
     // log available conversation
     const conversations = await api.getConversations();
     console.log('Available conversations');
     console.log(JSON.stringify(conversations, null, 2));
 
     // Log every event
+    api.on("event", (event) => {
+        if (event.resourceType === 'NewMessage') {
+            onNewMessage(event);
+        }
+    });
     api.on("Text", onMessage);
     api.on("RichText", onMessage);
+    api.on("NewMessage", onNewMessage);
 
     await api.listen();
 
+    function onNewMessage(event) {
+        const content = event.resource.content;
+        const toId = event.resource.conversation;
+        const toThread = event.resource.native.threadtopic;
+        const from = event.resource.native.imdisplayname;
+
+        const triggeredIntegration = integrations.pipes.find(integration =>
+            (!toThread && toId.toLowerCase().includes(integration.from.toLowerCase()))
+            || (toThread && toThread.toLowerCase().includes(integration.from.toLowerCase()))
+        );
+
+        if (triggeredIntegration) {
+            console.log(`Sent a message to the target in the following integration: ${JSON.stringify(triggeredIntegration)}`);
+            (async () => {
+                console.log(`Try to send message from ${from}`);
+                await send(from, content, triggeredIntegration)
+            })();
+
+        }
+    }
+
     function onMessage(event) {
-        console.log(JSON.stringify(event));
+        if (event.type != 'RichText' && event.type != 'Text') {
+            // wrong message type - just skip
+            return;
+        }
+
         // new messages
         const fromId = event.conversation;
         const fromUser = event.native.imdisplayname;
@@ -40,7 +73,7 @@ const password = process.env.SKYPE_PASSWORD;
         );
 
         if (triggeredIntegration) {
-            console.log(`Got a message for the following integration${JSON.stringify(triggeredIntegration)}`);
+            console.log(`Got a message for the following integration: ${JSON.stringify(triggeredIntegration)}`);
             (async () => {
                 const userTitle = getUserTitle(fromUser, fromThread);
                 console.log(`Try to send message from ${userTitle}`);
