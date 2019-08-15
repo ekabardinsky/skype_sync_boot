711b3765ed10b08eacb0383ad310b89a532ad7b2
Evgeny Kabardinsky <ekabardinsky@gmail.com>
2019-08-15 13:14:40 +0500

    Update typo

--- SkypeAdapter.js
+++ SkypeAdapter.js
@@ -107,7 +107,7 @@ class SkypeAdapter {
     async send(from, integration, event) {
         // check targets
         if (!integration.slackWebHook && integration.skypeTarget) {
-            Console.log(`No target specified for the following integration: ${JSON.stringify(integration)}`);
+            console.log(`No target specified for the following integration: ${JSON.stringify(integration)}`);
             return;
         }
 
